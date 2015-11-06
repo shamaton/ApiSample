f3abe3da0ae6f01a119fbf5b4e134a9bea33c1c2
Masayuki Shamoto <syamoto.masayuki@donuts.ne.jp>
2015-11-06 15:31:05 +0900

    改善できそうな実装テスト

---
+++ src/sample/model2/user.go
@@ -0,0 +1,41 @@
+package model2
+
+import (
+	"github.com/go-xorm/xorm"
+	"sample/DBI"
+	"github.com/gin-gonic/gin"
+)
+
+
+// table
+type User struct {
+	Id    int    `xorm:"pk"`
+	Name  string `xorm:"pk"`
+	Score int
+}
+
+type UserRepo interface {
+	FindByID(int) (*User, error)
+}
+
+func NewUserRepo(c *gin.Context) UserRepo {
+	tx, _ := DBI.GetDBSession(c)
+	db, _ := DBI.GetDBConnection(c, "user")
+
+	return UserRepoImpl{ db:db, tx:tx }
+}
+
+type UserRepoImpl struct {
+	db *xorm.Engine
+	tx *xorm.Session
+}
+
+func (r UserRepoImpl) FindByID(id int) (*User, error) {
+	user := new(User)
+	var err error
+
+	user.Id = id
+	_, err = r.db.Get(user)
+
+	return user, err
+}
