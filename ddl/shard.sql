c03a200fcba9b7f4740adac29409fd463ba1954e
Masayuki Shamoto <syamoto.masayuki@donuts.ne.jp>
2015-11-09 20:09:05 +0900

    userテーブルに時間を追加

--- ddl/shard.sql
+++ ddl/shard.sql
@@ -3,14 +3,16 @@
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE `user` (
-  id int(11) NOT NULL,
+  id int(11) unsigned NOT NULL,
   name varchar(255),
-  score int(11) NOT NULL,
+  score int(11) unsigned NOT NULL,
+  created_at int(11) unsigned NOT NULL,
+  updated_at int(11) unsigned NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 BEGIN;
-INSERT INTO user VALUES (1, "aaa", 100);
-INSERT INTO user VALUES (2, "bbb", 70);
-INSERT INTO user VALUES (3, "ccc", 50);
+INSERT INTO user VALUES (1, "aaa", 100, 123, 123);
+INSERT INTO user VALUES (2, "bbb", 70, 123, 123);
+INSERT INTO user VALUES (3, "ccc", 50, 123, 123);
 COMMIT;
